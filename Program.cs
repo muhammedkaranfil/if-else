1072a0099fb6d4335b9b7866f316caf82bc84626
muhammed abdülhalim kurunful <muhammedkaranfil6991@gmail.com>
2022-02-15 23:34:45 +0300

    net.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace  İfElseİF // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int  time = DateTime.Now.Hour;
+        
+        if(time>=6 && time<11)
+            Console.WriteLine("Günaydın");
+            else if(time<= 18)
+               Console.WriteLine("İyi Günler");
+               else
+                  Console.WriteLine("İyi Geceler");
+
+                string sonuc =time<=18 ? "iyi Günler!":  "İyi Geceler";
+                
+                  sonuc = time>=6 && time<=11 ? "Günaydın": time<= 18 ? "İyi Günler" :"İyi Geceler";
+
+                  Console.WriteLine(sonuc);  
+        }
+    }
+}
